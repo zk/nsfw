91af27e04573384c539edf92c5c43711ad9c4cb7
zkim <zachary.kim@gmail.com>
2011-06-27 04:42:05 +0000

    Rollback to congomongo 1.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
                  [clj-stacktrace "0.2.0"]
                  [hiccup "0.3.5"]
-                 [congomongo "0.1.5-SNAPSHOT"]
+                 [congomongo "0.1.4-SNAPSHOT"]
                  [ring "0.3.8"]
                  [net.cgrand/moustache "1.0.0"]
                  [nstools "0.2.4"]
