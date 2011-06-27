12c6bb5aab6dbeda2b5e80c8350b4d1a01ca8158
zkim <zachary.kim@gmail.com>
2011-06-27 01:46:48 +0000

    Updated deps, bump to 0.2

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject nsfw "0.1-SNAPSHOT"
+(defproject nsfw "0.2"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-stacktrace "0.2.0"]
-                 [hiccup "0.3.0"]
                  [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
-                 [fleet "0.9.3"]
-                 [congomongo "0.1.3-SNAPSHOT"]]
+                 [clj-stacktrace "0.2.0"]
+                 [hiccup "0.3.5"]
+                 [congomongo "0.1.5-SNAPSHOT"]
+                 [nstools "0.2.4"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
-                     [marginalia "0.5.0-alpha"]
                      [lein-clojars "0.6.0"]])
 
 
