5e0aaa351ee4f8d8d33b7301ee48fdde03d5e2ff
zkim <zachary.kim@gmail.com>
2011-06-27 04:47:17 +0000

    Bump to 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.2"
+(defproject nsfw "0.2.1"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
