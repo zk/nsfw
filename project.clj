2252b02e4df0e322eef0099cd879b20dc4d01b95
zkim <zachary.kim@gmail.com>
2011-06-29 03:19:06 +0000

    Reworked server.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.2.2"
+(defproject nsfw "0.2.3"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
--- src/nsfw/server.clj
+++ src/nsfw/server.clj
@@ -1,18 +1,43 @@
 (ns nsfw.server
+  "Easily manage jetty instances.
+
+   Usage:
+   ;; Create a server
+   (def s (make my-entry-handler :port 9090))
+
+   ;; Start server
+   (start s)
+
+   ;; Stop server
+   (stop s)
+
+   ;; Restart a server
+   (restart s)"
   (:require [ring.adapter.jetty :as jetty]))
 
-(def server nil)
+(def opts-defaults
+  {:port 8080
+   :join? false})
 
-(defn stop []
-  (when server
-    (.stop server)))
+(defn make [entry-handler & opts]
+  (let [opts (->> opts
+                  (apply hash-map)
+                  (merge opts-defaults))]
+    (atom {:opts opts
+           :handler entry-handler
+           :server nil})))
 
-(defn start [entry-handler & [port]]
-  (stop)
-  (alter-var-root
-   (var server)
-   (fn [val]
-     (jetty/run-jetty entry-handler {:port (if port port 8080) :join? false}))))
+(defn stop [server-atom]
+  (when-let [server (:server @server-atom)]
+    (.stop server))
+  (swap! server-atom assoc :server nil))
 
 
+(defn restart [server-atom]
+  (stop server-atom)
+  (let [handler (:handler @server-atom)
+        opts (:opts @server-atom)]
+    (swap! server-atom
+           assoc :server (jetty/run-jetty handler opts))))
 
+(def start restart)
