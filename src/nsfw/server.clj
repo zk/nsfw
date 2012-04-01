f49a50c7505ff4fd5e45f668354dd92597b70819
zkim <zachary.kim@gmail.com>
2012-03-31 19:36:51 -0700

    Cleaned up server ns.

--- src/nsfw/server.clj
+++ src/nsfw/server.clj
@@ -1,58 +1,36 @@
 (ns nsfw.server
   "Easily manage jetty instances.
 
-   Usage:
-   ;; Create a server
-   (def s (make my-entry-handler :port 9090))
+   Example:
+   (def server (atom nil))
+   (start-server server (fn [req] {:body \"hi\"}) {:port 4321})
 
-   ;; Start server
-   (start s)
+   ;; Change to port 4000 and join. Will shut down server bound to
+   ;; 4321.
+   (start-server server (fn [req] {:body \"lo\"}) {:port 4000 :join? true})"
+  (:require [ring.adapter.jetty :as jetty]))
 
-   ;; Stop server
-   (stop s)
 
-   ;; Restart a server
-   (restart s)"
-  (:require [ring.adapter.jetty :as jetty])
-  (:import [org.mortbay.thread QueuedThreadPool]))
+;; # Server
 
-(def opts-defaults
-  {:port 8080
-   :join? false})
+(def server-defaults {:port 8080 :join? false :max-threads 100})
 
-(defn make [entry-handler & opts]
-  (let [opts (->> opts
-                  (apply hash-map)
-                  (merge opts-defaults))]
-    (atom {:opts opts
-           :handler entry-handler
-           :server nil})))
+(defn start-server
+  "Start a jetty server. Opts include:
+   {:port        8080
+    :join?       false  ; Join server thread?
+    :max-threads 100    ; Request thredpool cap.
+    :min-threads 10}   
+"
+  [server-atom entry-point & [opts]]
+  (let [opts (merge server-defaults opts)]
+    (when @server-atom
+      (.stop @server-atom))
+    (swap! server-atom
+           (fn [& _] (jetty/run-jetty entry-point opts)))
+    (doto (.getThreadPool @server-atom)
+      (.setMaxThreads (:max-threads opts))
+      (.setMinThreads (:min-threads opts)))
+    server-atom))
 
-(defn set-threads [server-atom]
-  (let [server (:server @server-atom)
-        {:keys [max-threads min-threads]}
-        (:opts @server-atom)]
-    (when server
-      (let [tp (.getThreadPool server)]
-        (when min-threads (.setMinThreads tp min-threads))
-        (when max-threads (.setMaxThreads tp max-threads))))))
-
-(defn stop [server-atom]
-  (when-let [server (:server @server-atom)]
-    (.stop server))
-  (swap! server-atom assoc :server nil))
-
-(defn restart [server-atom & override-opts]
-  (let [override-opts (apply hash-map override-opts)
-        opts (merge (:opts @server-atom)
-                    override-opts)]
-    (stop server-atom)
-    (let [handler (:handler @server-atom)
-          res (swap! server-atom
-                     assoc
-                     :opts opts
-                     :server (jetty/run-jetty handler opts))]
-      (set-threads server-atom)
-      res)))
-
-(def start restart)
+(def restart-server start-server)
