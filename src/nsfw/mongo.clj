49443a6cb488a1bcecca8a3421fb4c9b4dce0d16
zkim <zachary.kim@gmail.com>
2012-04-14 19:29:27 -0700

    Added helper to coerce strings into bson ids.

--- src/nsfw/mongo.clj
+++ src/nsfw/mongo.clj
@@ -1,2 +1,6 @@
 (ns nsfw.mongo
-  (:use [somnium.congomongo]))
+  (:use [somnium.congomongo])
+  (:require [nsfw.util :as util]))
+
+(defn bson-id [id-or-str]
+  (org.bson.types.ObjectId/massageToObjectId id-or-str))
