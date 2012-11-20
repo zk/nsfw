6009df312cd43fa068496ffbffaf7d4f37689655
zkim <zachary.kim@gmail.com>
2012-11-19 21:59:59 -0800

    Bump deps, bump version to  0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject nsfw "0.3.0-SNAPSHOT"
+(defproject nsfw "0.4.0-SNAPSHOT"
   :description "No Such Framework -- Experimental"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-stacktrace "0.2.4"]
-                 [hiccup "1.0.0-beta1"]
-                 [congomongo "0.1.8"]
-                 [ring "1.1.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-stacktrace "0.2.5"]
+                 [hiccup "1.0.2"]
+                 [congomongo "0.3.3"]
+                 [ring "1.1.6"]
                  [net.cgrand/moustache "1.1.0"]
                  [slingshot "0.10.2"]
-                 [clj-http "0.3.5"]])
+                 [clj-http "0.5.8"]])
 
 
