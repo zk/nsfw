c97fcc0563573d8c54065bfd75a8dc48b1080862
zkim <zachary.kim@gmail.com>
2012-11-21 22:09:55 -0800

    Added test for parse-mongo-url

--- test/nsfw/test/mongo.clj
+++ test/nsfw/test/mongo.clj
@@ -10,3 +10,11 @@
   (is (= "bar" (parse-password (java.net.URI. "http://foo:bar@zaarly.com"))))
   (is (= nil   (parse-password (java.net.URI. "http://zaarly.com")))))
 
+(deftest test-parse-mongo-url
+  (is (= {:username "user"
+          :password "pass"
+          :host "host"
+          :port 123
+          :db "db"}
+         (parse-mongo-url "mongodb://user:pass@host:123/db"))))
+
