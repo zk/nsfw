f94442b9301b4cb929e16ee4f8545a4d3690edad
zk <zachary.kim@gmail.com>
2013-01-12 13:05:24 -0800

    Removed unused assets

--- resources/nsfw/routes.tpl.clj
+++
@@ -1,7 +0,0 @@
-(ns <(str project-name)>.routes
-  (:use [net.cgrand.moustache :only (app)]
-        [hiccup core]
-        [nsfw render]))
-
-(def routes
-  (app [""] (fn [r] (render :text "hello from nsfw."))))
