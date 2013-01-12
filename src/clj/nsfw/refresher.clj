70c6ae1fa784ba0249e2c71adf105cee3eae1d19
zk <zachary.kim@gmail.com>
2013-01-12 13:49:50 -0800

    Embedded safari osascript directly into clojure, deleted shell scripts

--- src/clj/nsfw/refresher.clj
+++ src/clj/nsfw/refresher.clj
@@ -2,16 +2,31 @@
   (:require [clojure.java.shell :as sh]
             [watchtower.core :as wt]))
 
+(defn refresh-safari [url-str-match]
+  (->> ["osascript"
+        "tell application \"Safari\""
+        "set windowList to every window"
+        "repeat with aWindow in windowList"
+        "set tabList to every tab of aWindow"
+        "repeat with atab in tabList"
+        (format "if (URL of atab contains \"%s\") then" url-str-match)
+        "tell atab to do javascript \"window.location.reload()\""
+        "end if"
+        "end repeat"
+        "end repeat"
+        "end tell"]
+       (interpose "-e")
+       (apply sh/sh)))
+
 (defn refresh-safari-on-js-change []
   (wt/watcher "resources/public"
               (wt/rate 100)
               (wt/on-change (fn [& args]
-                              (println
-                               (->> args
-                                    (interpose ", ")
-                                    (apply str))
-                               "changed, reloading")
-                              (sh/sh "./bin/reload-safari")))))
+                              (println (->> args
+                                            (interpose ", ")
+                                            (apply str))
+                                       "changed, refreshing safari")
+                              (refresh-safari "localhost")))))
 
 (defn -main [& args]
   (refresh-safari-on-js-change))
