19468194d017459f867df1c2da2019c17f8ecb14
zk <zachary.kim@gmail.com>
2013-01-22 22:10:14 -0800

    Added lein-template to deploy script

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/zkim/nsfw"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :jar-name "lein-template.jar")
