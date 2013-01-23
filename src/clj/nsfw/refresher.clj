5c4b34487648a44f5329537c790026312fa0a3d9
zk <zachary.kim@gmail.com>
2013-01-22 17:25:14 -0800

    Added chrome refresh script.

--- src/clj/nsfw/refresher.clj
+++ src/clj/nsfw/refresher.clj
@@ -18,15 +18,28 @@
        (interpose "-e")
        (apply sh/sh)))
 
-(defn refresh-safari-on-js-change []
+(defn refresh-chrome
+  "Refreshes first tab of chrome"
+  []
+  (->> ["osascript"
+        "tell application \"Google Chrome\" to tell the active tab of its first window"
+        "reload"
+        "end tell"]
+       (interpose "-e")
+       (apply sh/sh)))
+
+(defn refresh-on-change [refresh-fn]
   (wt/watcher ["resources/public" "src/clj"]
               (wt/rate 100)
               (wt/on-change (fn [& args]
                               (println (->> args
                                             (interpose ", ")
                                             (apply str))
-                                       "changed, refreshing safari")
-                              (refresh-safari "localhost")))))
+                                       "changed, refreshing browser")
+                              (refresh-fn)))))
 
-(defn -main [& dirs]
-  (refresh-safari-on-js-change))
+(defn -main [& [browser]]
+  (condp = browser
+    "safari" (refresh-on-change #(refresh-safari "localhost"))
+    (refresh-on-change refresh-chrome) ; default to chrome
+    ))
