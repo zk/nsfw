ce7aff3f599b1e35f1faa165819c15866c9ff2cb
zk <zachary.kim@gmail.com>
2013-02-27 19:53:03 -0800

    Fix error when google maps is not available

--- src/cljs/nsfw/geo.cljs
+++ src/cljs/nsfw/geo.cljs
@@ -5,6 +5,8 @@
 
 (def -geolocation (.-geolocation js/navigator))
 
+(def goog-loaded (not (nil? (aget js/window "google"))))
+
 (defn geoloc->map [g]
   (let [c (.-coords g)]
     {:timestamp (.-timestamp g)
@@ -26,28 +28,30 @@
 (defn map
   "Map options, see http://goo.gl/IlBC8 for full list."
   [el & [opts]]
-  (let [opts (assoc opts :center
-                    (if (:center opts)
-                      (google.maps.LatLng. (first (:center opts)) (second (:center opts)))
-                      (google.maps.LatLng. 37.7750 -122.4183)))]
-    (google.maps.Map. el (clj->js
-                          (merge {:zoom 2
-                                  :mapTypeId google.maps.MapTypeId.ROADMAP
-                                  :scrollwheel false
-                                  :panControl false
-                                  :zoomControl true
-                                  :mapTypeControl false
-                                  :scaleControl false
-                                  :streetViewControl false
-                                  :overviewMapControl false}
-                                 opts)))))
+  (when goog-loaded
+    (let [opts (assoc opts :center
+                      (if (:center opts)
+                        (google.maps.LatLng. (first (:center opts)) (second (:center opts)))
+                        (google.maps.LatLng. 37.7750 -122.4183)))]
+      (google.maps.Map. el (clj->js
+                            (merge {:zoom 2
+                                    :mapTypeId google.maps.MapTypeId.ROADMAP
+                                    :scrollwheel false
+                                    :panControl false
+                                    :zoomControl true
+                                    :mapTypeControl false
+                                    :scaleControl false
+                                    :streetViewControl false
+                                    :overviewMapControl false}
+                                   opts))))))
 
 (defn center
   ([map]
      (let [center (.getCenter map)]
        [(.lat center) (.lng center)]))
   ([map [lat lng]]
-     (.setCenter map (google.maps.LatLng. lat lng))))
+     (when goog-loaded
+       (.setCenter map (google.maps.LatLng. lat lng)))))
 
 (defn zoom [map level]
   (.setZoom map level))
