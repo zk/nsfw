3a44c17a4732b5517966bd7098bd52a6a2e0518c
zk <zachary.kim@gmail.com>
2013-02-27 19:48:42 -0800

    Load prefs from $PROJ_ROOT/.refresher, refresh first tab in chrome, not top-most.

--- src/clj/nsfw/refresher.clj
+++ src/clj/nsfw/refresher.clj
@@ -2,6 +2,17 @@
   (:require [clojure.java.shell :as sh]
             [watchtower.core :as wt]))
 
+(defn slurp-safe [path]
+  (try
+    (slurp path)
+    (catch java.io.FileNotFoundException e
+      nil)))
+
+(defn config-contents [& [file-path]]
+  (let [file-path (or file-path ".refresher")
+        contents (slurp-safe file-path)]
+    contents))
+
 (defn refresh-safari []
   (->> ["osascript"
         "tell application \"Safari\" to tell its first document"
@@ -14,25 +25,28 @@
   "Refreshes first tab of chrome"
   []
   (->> ["osascript"
-        "tell application \"Google Chrome\" to tell the active tab of its first window"
+        "tell application \"Google Chrome\" to tell the first tab of its first window"
         "reload"
         "end tell"]
        (interpose "-e")
        (apply sh/sh)))
 
 (defn refresh-on-change [refresh-fn]
-  (wt/watcher ["resources/public" "src/clj"]
-              (wt/rate 100)
-              (wt/on-change (fn [args]
-                              (println (->> args
-                                            (map #(.getPath %))
-                                            (interpose ", ")
-                                            (apply str))
-                                       "changed, refreshing browser")
-                              (refresh-fn)))))
+  (wt/watcher
+   ["resources/public" "src/clj"]
+   (wt/rate 100)
+   (wt/on-change (fn [args]
+                   (println (->> args
+                                 (map #(.getPath %))
+                                 (interpose ", ")
+                                 (apply str))
+                            "changed, refreshing browser")
+                   (refresh-fn))))
+  nil)
 
-(defn -main [& [browser]]
-  (condp = browser
-    "safari" (refresh-on-change refresh-safari)
-    (refresh-on-change refresh-chrome) ; default to chrome
-    ))
+(defn -main []
+  (let [browser (or (:browser (config-contents))
+                    "chrome")]
+    (condp = browser
+      "safari" (refresh-on-change refresh-safari)
+      "chrome" (refresh-on-change refresh-chrome))))
