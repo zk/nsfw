9e135ccba21742c47f51f11517bcff44713d0662
zk <zachary.kim@gmail.com>
2013-03-06 22:37:33 -0800

    Added unique value blurb to gen'd page

--- lein-template/src/leiningen/new/nsfw/app.cljs
+++ lein-template/src/leiningen/new/nsfw/app.cljs
@@ -1,5 +1,4 @@
 (ns {{name}}.app
-  (:use [nsfw.util :only [log]])
   (:require [nsfw.dom :as dom]
             [nsfw.bind :as bind]
             [nsfw.util :as util]))
@@ -12,7 +11,8 @@
    [:p
     "This file can be found at " [:code "src/cljs/{{sanitized}}/app.cljs"] ". "
     "Any changes to this file (and any other cljs files) will automatically be "
-    "reloaded in your browser."]])
+    "reloaded in your browser."]
+   [:p "The unique value embedded from the backend is: " (util/page-data :unique-value)]])
 
 (defn ^:export entry []
   (dom/append $body $page))
