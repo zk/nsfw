854fb0ee3fcaf0f4ffba4b57288b8205ad8fa53a
zk <zachary.kim@gmail.com>
2013-04-17 13:29:11 -1000

    Clean up and add debug-exceptions to run

--- lein-template/src/leiningen/new/nsfw/run.clj
+++ lein-template/src/leiningen/new/nsfw/run.clj
@@ -1,19 +1,30 @@
 (ns {{name}}.run
   (:require [nsfw.server :as server]
             [nsfw.env :as env]
+            [nsfw.app :as app]
             [ring.middleware.reload-modified :as reload]
             [{{name}}.entry :as entry]
             [clojure.tools.nrepl.server :as repl]))
 
-(defn start-repl [port]
-  (repl/start-server :port port))
+;; Config from environment variables
 
+(def debug-exceptions? (env/bool :debug-exceptions false))
+(def repl-port (env/int :repl-port))
+(def server-port (env/int :port 8080))
+
+
+(defn start-repl [port]
+  (when port
+    (repl/start-server :port port)))
+
+;; Request handling
 (def root-entry
   (-> #'entry/app
-      (reload/wrap-reload-modified ["src/clj"])))
+      (reload/wrap-reload-modified ["src/clj"])
+      (app/debug-exceptions debug-exceptions?)))
 
+;; Main
 (defn -main [& args]
-  (when-let [repl-port (env/int :repl-port)]
-    (start-repl repl-port))
+  (start-repl repl-port)
   (server/start :entry root-entry
-                :port (env/int :port 8080)))
+                :port server-port))
