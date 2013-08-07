e2a33d0b3a360ff539510548e038281c80ab1aa7
zk <zachary.kim@gmail.com>
2013-08-06 17:47:39 -0700

    Add component alpha to html

--- src/clj/nsfw/html.clj
+++ src/clj/nsfw/html.clj
@@ -1,6 +1,7 @@
 (ns nsfw.html
   (:require [hiccup.core :as hiccup]
-            [hiccup.page :as hiccup-page])
+            [hiccup.page :as hiccup-page]
+            [clojure.zip :as zip])
   (:import [org.pegdown PegDownProcessor]
            [org.pegdown Parser]))
 
@@ -53,3 +54,85 @@
 (defn markdown [s]
   (let [pd (PegDownProcessor.)]
       (.markdownToHtml pd s)))
+
+
+;; Components
+(defn parse-comp [[tag opts-raw & body]]
+  (let [opts (if (map? opts-raw)
+               opts-raw
+               {})
+        body (if (map? opts-raw)
+               body
+               (concat [opts-raw] body))]
+    [opts body]))
+
+(defn tag-match? [!components node]
+  (and (coll? node)
+       (keyword? (first node))
+       (@!components (first node))))
+
+
+(defn vec-or-seq? [o]
+  (or (seq? o)
+      (vector? o)))
+
+(defn nested-seq? [o]
+  (and (vec-or-seq? o)
+       (vec-or-seq? (first o))))
+
+(defn has-nested-seqs? [node]
+  (when (vec-or-seq? node)
+    (some nested-seq? node)))
+
+(defn unwrap-nested-seqs [node]
+  (let [nested-seqs (filter nested-seq? node)]
+    (loop [nested-seqs nested-seqs
+           node node]
+      (if (empty? nested-seqs)
+        node
+        (recur (rest nested-seqs)
+               (let [ns (first nested-seqs)
+                     pre (take-while #(not= ns %) node)
+                     post (->> node
+                               (drop-while #(not= ns %))
+                               (drop 1))]
+                 (vec (concat pre ns post))))))))
+
+(defn apply-comps [!components structure]
+  (let [z (zip/zipper #(or (vector? %) (seq? %))
+                      identity
+                      (fn [node children]
+                        (with-meta (vec children) (meta node)))
+                      structure)]
+    (loop [n z]
+      (if (zip/end? n)
+        (zip/root n)
+        (recur (zip/next
+                (cond
+                 (tag-match? !components (zip/node n))
+                 (zip/edit n (fn [node]
+                               (let [tag (first node)
+                                     [opts body] (parse-comp node)
+                                     f (@!components tag)
+                                     res (if (fn? f)
+                                           (f opts body)
+                                           f)
+                                     res (apply-comps res)]
+                                 res)))
+
+                 (has-nested-seqs? (zip/node n))
+                 (zip/edit n unwrap-nested-seqs)
+
+                 :else n)))))))
+
+(defn mk-comp [!components]
+  (fn comp [& comps]
+    (->> comps
+         (partition 2)
+         (reduce #(assoc %1 (first %2) (second %2)) {})
+         (swap! !components merge))))
+
+(defn mk-transformer [!components]
+  (fn [body-coll]
+    (-> (map #(apply-comps !components %) body-coll)
+        html5)))
