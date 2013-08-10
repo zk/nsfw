f3bb432ea2a81a1eab8a9a72f2cdf63738adf6ce
zk <zachary.kim@gmail.com>
2013-08-09 22:41:02 -0700

    Bump to 0.6.10

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw/lein-template "0.6.9"
+(defproject nsfw/lein-template "0.6.10"
   :description "Template for NSFW apps."
   :url "http://github.com/zkim/nsfw"
   :license {:name "Eclipse Public License"
