d9e5d70e73ba6586afa9eb239805fa754b40a0d0
zk <zachary.kim@gmail.com>
2013-08-22 21:32:05 -0700

    Remove lein-template

--- lein-template/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject nsfw/lein-template "0.6.11"
-  :description "Template for NSFW apps."
-  :url "http://github.com/zkim/nsfw"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true
-  :jar-name "lein-template.jar")
