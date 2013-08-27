125e57e930819ee6940cb69bae9ce6033d9c4847
zk <zachary.kim@gmail.com>
2013-08-26 23:55:50 -0700

    Bump to 0.6.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.6.16"
+(defproject nsfw "0.6.17"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-stacktrace "0.2.5"]
