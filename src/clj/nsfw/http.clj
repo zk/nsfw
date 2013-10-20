99bb41c7de68751341341281f6492782d62ab660
zk <zachary.kim@gmail.com>
2013-10-19 18:07:14 -1000

    Move decode to http ns, add decode edn midleware

--- src/clj/nsfw/http.clj
+++ src/clj/nsfw/http.clj
@@ -1,7 +1,8 @@
 (ns nsfw.http
   "Request / response utilities"
   (:use [plumbing.core])
-  (:require [plumbing.graph :as pg]))
+  (:require [plumbing.graph :as pg]
+            [clojure.edn :as edn]))
 
 (defn -graph [gmap strategy]
   (let [g (strategy gmap)]
@@ -35,3 +36,38 @@
    {:headers {"Location" loc}
     :status 301}
    (apply hash-map opts)))
+
+(defn decode-body [content-length body]
+  (when (and content-length
+             (> content-length 0))
+    (let [buf (byte-array content-length)]
+      (.read body buf 0 content-length)
+      (.close body)
+      (String. buf))))
+
+(defn response-body
+  "Turn a HttpInputStream into a string."
+  [{:keys [content-length body]}]
+  (if (string? body)
+    body
+    (decode-body content-length body)))
+
+(defn decode-edn-body [r]
+  (-> r response-body edn/read-string))
+
+(defn decode-edn-mw [h]
+  (fn [r]
+    (let [content-type-raw (-> r :headers (get "content-type"))]
+      (if (and content-type-raw
+               (.contains content-type-raw "application/edn"))
+        (h (assoc r :edn-body (decode-edn-body r)))
+        (h r)))))
+
+(defn tag-response-mw
+  "Middleware for attaching metadata to a response.
+   Usage: (-> h (tag-response-mw :handler-ns 'nsfw.http) ...)"
+  [h k v]
+  (fn [r]
+    (let [resp (h r)]
+      (when resp
+        (vary-meta resp assoc k v)))))
