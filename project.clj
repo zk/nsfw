6fbc8faa3b6305eb092ecddd4a302c268c881122
Zachary Kim <zachary.kim@gmail.com>
2015-03-01 00:30:36 -0800

    Bump to 0.8.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.8.9"
+(defproject nsfw "0.8.10"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2665"]
