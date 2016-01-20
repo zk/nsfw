4e594742116a966f2adda8b274c3cf01c2483c56
Zachary Kim <zachary.kim@gmail.com>
2016-01-20 00:16:58 -0800

    Add hammerjs

--- src/cljs/deps.cljs
+++ src/cljs/deps.cljs
@@ -1,4 +1,8 @@
 {:foreign-libs [{:file     "dropzone/dropzone.js"
                  :file-min "dropzone/dropzone.min.js"
-                 :provides ["com.dropzonejs"]}]
- :externs ["dropzone/externs/dropzone.js"]}
+                 :provides ["com.dropzonejs"]}
+                {:file     "hammerjs/hammer.js"
+                 :file-min "hammerjs/hammer.min.js"
+                 :provides ["com.hammerjs"]}]
+ :externs ["dropzone/externs/dropzone.js"
+           "hammerjs/externs/hammer.js"]}
---
+++ src/cljs/hammerjs/externs/hammer.js
@@ -0,0 +1,19 @@
+var Hammer = function(el, opts);
+
+Hammer.DIRECTION_VERTICAL = {};
+
+Hammer.Manager = function(el, opts){};
+Hammer.Manager.set = function(opts){};
+Hammer.Manager.get = function(o){};
+Hammer.Manager.add = function(o){};
+Hammer.Manager.remove = function(o){};
+Hammer.Manager.on = function(e,h){};
+Hammer.Manager.off = function(e,h){};
+Hammer.Manager.stop = function(o){};
+
+Hammer.Recognizer = function(o){};
+Hammer.Recognizer.set = function(o){};
+Hammer.Recognizer.recognizeWith = function(o){};
+Hammer.Recognizer.dropRecognizeWith = function(o){};
+Hammer.Recognizer.requireFailure = function(o){};
+Hammer.Recognizer.dropRequireFailure = function(o){};
