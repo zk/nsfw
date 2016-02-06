faec6396f0b8bc8d61d81a484434c77f44cf0f35
Zachary Kim <zachary.kim@gmail.com>
2016-02-05 18:27:53 -0800

    Remove hammer

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.11.26"
+(defproject nsfw "0.11.27"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
