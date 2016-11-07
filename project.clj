344dafddc25338d38f449126f17155a211c0ffe4
Zachary Kim <zachary.kim@gmail.com>
2016-11-07 16:26:14 -0500

    Add page/hook-reload-fn

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.11.45"
+(defproject nsfw "0.11.46"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.122"]
