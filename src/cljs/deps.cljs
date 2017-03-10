f4d5c6c83a9c573d68ede7e1cc0ac62bb134d3ea
Zachary Kim <zachary.kim@gmail.com>
2017-03-09 21:36:23 -1000

    Remove dropzone

--- src/cljs/deps.cljs
+++
@@ -1,4 +0,0 @@
-{:foreign-libs [{:file     "dropzone/dropzone.js"
-                 :file-min "dropzone/dropzone.min.js"
-                 :provides ["com.dropzonejs"]}]
- :externs ["dropzone/externs/dropzone.js"]}
