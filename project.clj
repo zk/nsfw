ac6ba87d6fdc843e283a2e5f20e3eb50ab6999b7
Zachary Kim <zachary.kim@gmail.com>
2017-04-06 23:51:04 -1000

    Bump nsfw

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.11.58"
+(defproject nsfw "0.11.59"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.122"]
