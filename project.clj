e5023ae03925cccca411f4040a790aa61e92197f
Zachary Kim <zachary.kim@gmail.com>
2018-09-19 11:57:09 -1000

    Bump 0.12.29

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nsfw "0.12.28"
+(defproject nsfw "0.12.29"
   :description "No Such Framework -- Experimental"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
